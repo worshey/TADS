112566c3f4d2a4ae9f69ccb150f9999a67222dd2
worshey <worshey154@gmail.com>
2025-09-17 20:11:45 -0300

    sim

--- Programação I/primeiroProjeto/primeiroCodigo/src/Venda.java
+++ Programação I/primeiroProjeto/primeiroCodigo/src/Venda.java
@@ -4,4 +4,24 @@ public class Venda {
     private LocalDate data;
     private Double valorTotal;
     private Produto produto;
+   
+    public LocalDate getData() {
+        return data;
+    }
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    public Produto getProduto() {
+        return produto;
+    }
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+    
 }
